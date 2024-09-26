59c01949e264ec90be020835e6fd2777e9e15286
KVXLIZ <70652179+KVXLIZ@users.noreply.github.com>
2024-09-26 11:33:11 +0200

    Added tree structure

--- tree.py
+++ tree.py
@@ -2,4 +2,19 @@ class Tree:
     def __init__(self):
         self.key = None
         self.children = []
-        
+    
+    #  Sets the key for node
+    def setKey(self, k):
+        self.key = k
+    
+    #  Returns the key
+    def getKey(self):
+        return self.key
+    
+    #  Appends a child to the list
+    def addChild(self, child):
+        self.children.append(child)
+    
+    #  Returns the children of the node
+    def getChildren(self):
+        return self.children
